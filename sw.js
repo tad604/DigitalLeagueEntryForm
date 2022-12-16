df75990c2b17b8583177e3030be1ede17a5c453b
Tad Dicks <tad604@users.noreply.github.com>
2022-12-16 08:55:00 -1000

    image updates
    larger icon

--- sw.js
+++ sw.js
@@ -7,17 +7,20 @@ self.addEventListener("install", (event) => {
   event.waitUntil(
     addResourcesToCache([
       "/",
-      "/index.html",
-      "/css/main.css",
-      "/css/normalize.css",
-      "/js/main.js",
-      "/img/vagrant.png",
-      "/img/Medium_Box.png",
-      "/img/root-background.jpg",
-      "/img/autumn.jpg",
-      "/img/baseDeck.gif",
-      "/img/ExilesAndPartisansDeck.gif",
-      "/img/winter.jpg"
+      "index.html",
+      "css/main.css",
+      "css/normalize.css",
+      "js/main.js",
+      "fonts/Luminari-Regular.woff",
+      "fonts/Baskerville Ten Pro Regular.otf",
+      "img/vagrant.png",
+      "img/vagrant-lg.png",
+      "img/Medium_Box.png",
+      "img/root-background.jpg",
+      "img/autumn.jpg",
+      "img/baseDeck.gif",
+      "img/ExilesAndPartisansDeck.gif",
+      "img/winter.jpg"
     ])
   );
 });
