bda1d0923296d59c31ce22fd412d364e9aaa807b
Tad Dicks <tad604@users.noreply.github.com>
2023-03-01 07:14:42 -1000

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-//GOOGLE_SHEET_URL = "https://script.google.com/macros/s/AKfycbxN-BfGk_NrEaKMNC3cWlISDyw4wqRkb-VsYRDZ3zd2U5E_eaTEbvxzdK5wY1jvPNVC/exec";
-const GOOGLE_SHEET_URL = "https://script.google.com/macros/s/AKfycbyVqdUZQNGKXJw2VpFvu2MVM5HcuqvZz7Qi-s5SlxznMjeV6ho5ucI1xxM3xOnVOWCJ/exec";
+const GOOGLE_SHEET_URL = "https://script.google.com/macros/s/AKfycbxN-BfGk_NrEaKMNC3cWlISDyw4wqRkb-VsYRDZ3zd2U5E_eaTEbvxzdK5wY1jvPNVC/exec";
+//const GOOGLE_SHEET_URL = "https://script.google.com/macros/s/AKfycbyVqdUZQNGKXJw2VpFvu2MVM5HcuqvZz7Qi-s5SlxznMjeV6ho5ucI1xxM3xOnVOWCJ/exec";
 const players = ['player1', 'player2', 'player3', 'player4'];
 const factionImages = {
   'Eyrie Dynasties':'Eyrie_Warrior.png',
